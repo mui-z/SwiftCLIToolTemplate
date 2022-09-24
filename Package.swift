276fe89ffd0242a213a6321f1f6577f44708e095
mui_z <93278577+mui-z@users.noreply.github.com>
2022-09-24 14:40:15 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftCLIToolTemplate",
-    platforms: [.macOS(.v10_13)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .executable(name: "test", targets: ["SwiftCLIToolTemplate"]),
     ],
